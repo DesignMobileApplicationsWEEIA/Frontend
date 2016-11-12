efb22365bd675768a3da4d40e0c5f8e632807cb8
msut <marr994@o2.pl>
2016-11-12 20:38:15 +0100

    add virtual campus walk api

---
+++ app/src/main/java/adm/virtualcampuswalk/utli/api/VirtualCampusWalk.java
@@ -0,0 +1,18 @@
+package adm.virtualcampuswalk.utli.api;
+
+import adm.virtualcampuswalk.models.Building;
+import adm.virtualcampuswalk.models.PhoneData;
+import adm.virtualcampuswalk.models.Result;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+/**
+ * Created by mariusz on 12.11.16.
+ */
+
+public interface VirtualCampusWalk {
+
+    @POST("/Building/search")
+    Call<Result<Building>> getBuilding(@Body PhoneData phoneData);
+}
