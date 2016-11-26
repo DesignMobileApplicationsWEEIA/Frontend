56ef6d10a5731094a9dda3e457670ee289f1d5a5
msut <marr994@o2.pl>
2016-11-26 17:53:30 +0100

    small changes

--- app/src/main/java/adm/virtualcampuswalk/fragments/CameraViewFragment.java
+++ app/src/main/java/adm/virtualcampuswalk/fragments/CameraViewFragment.java
@@ -71,15 +71,15 @@ public class CameraViewFragment extends PositionServiceFragment {
         initArrowUtils(inflate);
         initVirtualCampusWalk();
         facultyLogo = (ImageView) inflate.findViewById(R.id.facultyLogo);
-//        exampleCall(new PhoneLocation(19.45301, 51.752497)); // WEEIA
-//        exampleCall(new PhoneLocation(19.455541, 51.745947)); // DMCS
-//        exampleCall(new PhoneLocation(19.455817, 51.747364)); // CTI
+        buildingCall(new PhoneData(100f, new PhoneLocation(19.45301, 51.752497))); // WEEIA
+//        buildingCall(new PhoneData(100f, new PhoneLocation(19.455541, 51.745947)));// DMCS
+//        buildingCall(new PhoneData(100f, new PhoneLocation(19.455817, 51.747364))); // CTI
         return inflate;
     }
 
-    private void exampleCall(PhoneLocation phoneLocation) {
+    private void buildingCall(PhoneData phoneData) {
 //        Call<Result<Building>> call = virtualCampusWalk.getBuilding(new PhoneData(positionSensorService.getPhoneRotation().getRoll(), phoneLocation));
-        Call<Result<Building>> call = virtualCampusWalk.getBuilding(new PhoneData(0.0, phoneLocation));
+        Call<Result<Building>> call = virtualCampusWalk.getBuilding(phoneData);
         call.enqueue(new Callback<Result<Building>>() {
             @Override
             public void onResponse(Call<Result<Building>> call, Response<Result<Building>> response) {
@@ -89,8 +89,8 @@ public class CameraViewFragment extends PositionServiceFragment {
             }
 
             @Override
-            public void onFailure(Call<Result<Building>> call, Throwable t) {
-                Log.e(TAG, "ERROR: " + t.getMessage());
+            public void onFailure(Call<Result<Building>> call, Throwable throwable) {
+                Log.e(TAG, "ERROR: " + throwable.getMessage(), throwable);
                 setDataFrameVisibility(false);
             }
         });
@@ -141,8 +141,9 @@ public class CameraViewFragment extends PositionServiceFragment {
         this.locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                Log.i(TAG, "NEW LOCATION " + "LAT: " + location.getLatitude() + " LON: " + location.getLongitude());
-                exampleCall(new PhoneLocation(location.getLongitude(), location.getLatitude()));
+                PhoneRotation phoneRotation = positionSensorService.getPhoneRotation();
+                Log.i(TAG, "PHONE ROTATION " + phoneRotation + " NEW LOCATION " + "LAT: " + location.getLatitude() + " LON: " + location.getLongitude());
+                buildingCall(new PhoneData(phoneRotation.getAzimuth(), new PhoneLocation(location.getLongitude(), location.getLatitude())));
             }
         };
     }
@@ -184,7 +185,7 @@ public class CameraViewFragment extends PositionServiceFragment {
             buildingData.add(body.getValue().getAddress());
 
             setListViewData(buildingData);
-            setimageData(body);
+            setImageData(body);
         } else {
             setDataFrameVisibility(false);
         }
@@ -195,7 +196,7 @@ public class CameraViewFragment extends PositionServiceFragment {
             TextView textView = (TextView) getActivity().findViewById(id);
             textView.setText(text);
         } catch (Exception ex) {
-            Log.e(TAG, "CameraViewFragment: setTextViewText: " + ex.getMessage());
+            Log.e(TAG, "CameraViewFragment: setTextViewText: " + ex.getMessage(), ex);
         }
     }
 
@@ -205,7 +206,7 @@ public class CameraViewFragment extends PositionServiceFragment {
         listView.setAdapter(arrayAdapter);
     }
 
-    private void setimageData(Result<Building> body) {
+    private void setImageData(Result<Building> body) {
         String imageBytes = body.getValue().getFaculties().get(0).getLogo().getContent();
         facultyLogo.setImageBitmap(Util.convertStringByteToBitmap(imageBytes));
         setDataFrameVisibility(true);
