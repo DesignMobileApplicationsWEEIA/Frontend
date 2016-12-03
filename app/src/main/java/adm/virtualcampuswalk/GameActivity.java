2525ffc5fcfcd1514d9a7595fef4b90a7bbff683
msut <marr994@o2.pl>
2016-12-03 14:19:27 +0100

    add game fragment activity

--- app/src/main/java/adm/virtualcampuswalk/GameActivity.java
+++ app/src/main/java/adm/virtualcampuswalk/GameActivity.java
@@ -1,14 +1,21 @@
 package adm.virtualcampuswalk;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        viewPager = (ViewPager) findViewById(R.id.game_pager);
+        PagerAdapter pagerAdapter = new GameFragmentActivity(getSupportFragmentManager(), getResources().getConfiguration());
+        viewPager.setAdapter(pagerAdapter);
     }
 
 }
---
+++ app/src/main/java/adm/virtualcampuswalk/GameFragmentActivity.java
@@ -0,0 +1,60 @@
+package adm.virtualcampuswalk;
+
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import adm.virtualcampuswalk.fragments.AchievementViewFragment;
+import adm.virtualcampuswalk.fragments.CameraViewFragment;
+import adm.virtualcampuswalk.fragments.MapViewFragment;
+
+/**
+ * Created by mariusz on 03.12.16.
+ */
+
+public class GameFragmentActivity extends FragmentStatePagerAdapter {
+
+    private Configuration conf;
+    private int numberOfFragments = 3;
+    private String fragmentTitle[] = {"AchievementViewFragment", "CameraViewFragment", "MapViewFragment"};
+
+    public GameFragmentActivity(FragmentManager fragmentManager, Configuration conf) {
+        super(fragmentManager);
+        this.conf = conf;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new AchievementViewFragment();
+            case 1:
+                return new CameraViewFragment();
+            case 2:
+                return new MapViewFragment();
+        }
+        return null;
+    }
+
+    @Override
+    public float getPageWidth(int position) {
+        float nbPages;
+        if (conf.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            nbPages = 1;
+        } else {
+            nbPages = 1;
+        }
+        return (1 / nbPages);
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfFragments;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle[position];
+    }
+}
