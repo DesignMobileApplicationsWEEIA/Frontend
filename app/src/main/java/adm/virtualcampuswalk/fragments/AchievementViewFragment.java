148a8a1e448d0618e662cd5a941996cb5a5dffc5
msut <marr994@o2.pl>
2016-12-03 14:18:52 +0100

    add achievement fragment

---
+++ app/src/main/java/adm/virtualcampuswalk/fragments/AchievementViewFragment.java
@@ -0,0 +1,10 @@
+package adm.virtualcampuswalk.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mariusz on 03.12.16.
+ */
+
+public class AchievementViewFragment extends Fragment {
+}
