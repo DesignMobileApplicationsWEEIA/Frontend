003bd62195a7234293e2073a51c48e38ec9e1e3c
msut <marr994@o2.pl>
2016-12-07 20:06:26 +0100

    add game camera view fragment building call

--- app/src/main/java/adm/virtualcampuswalk/fragments/game/GameCameraViewFragment.java
+++ app/src/main/java/adm/virtualcampuswalk/fragments/game/GameCameraViewFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,6 +33,8 @@ import adm.virtualcampuswalk.utli.arrow.ArrowUpdater;
 import adm.virtualcampuswalk.utli.arrow.SimpleArrowUpdater;
 import adm.virtualcampuswalk.utli.camera.CameraPreview;
 import adm.virtualcampuswalk.utli.gps.LocationService;
+import adm.virtualcampuswalk.utli.network.MacReader;
+import adm.virtualcampuswalk.utli.network.SimpleMacReader;
 import adm.virtualcampuswalk.utli.rotation.RotationReader;
 import adm.virtualcampuswalk.utli.rotation.SimpleRotationReader;
 import retrofit2.Call;
@@ -50,7 +53,7 @@ import static adm.virtualcampuswalk.utli.camera.CameraService.setPosition;
  * Created by Adam Piech on 2016-10-20.
  */
 public class GameCameraViewFragment extends GamePositionServiceFragment {
-
+    private static int DELAY = 1000;
     private Camera camera;
     private CameraPreview preview;
     private Camera.Parameters parameters;
@@ -60,7 +63,17 @@ public class GameCameraViewFragment extends GamePositionServiceFragment {
     private RotationReader rotationReader;
     private ArrowUpdater arrowUpdater;
     private ImageView arrow;
+    private ImageView facultyLogo;
     private VirtualCampusWalk virtualCampusWalk;
+    private MacReader mac = new SimpleMacReader();
+    private Location lastLocation;
+
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        public void run() {
+            initBuildingCalls();
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -68,23 +81,41 @@ public class GameCameraViewFragment extends GamePositionServiceFragment {
         View inflate = inflater.inflate(R.layout.camera_view_activity, container, false);
         initLocationRequests();
         initArrowUtils(inflate);
-        initVirtualCampusGame();
+        initVirtualCampusWalk();
+        facultyLogo = (ImageView) inflate.findViewById(R.id.facultyLogo);
         return inflate;
     }
 
+    private void initBuildingCalls() {
+        if (lastLocation != null) {
+            Log.i(TAG, "REQUEST CALL");
+            double azimuth = positionSensorService.getPhoneRotation().getAzimuth();
+            PhoneData phoneData = new PhoneData(azimuth, new PhoneLocation(lastLocation.getLongitude(), lastLocation.getLatitude()), mac.getMacAddress());
+            Log.d(TAG, "initBuildingCalls: " + phoneData);
+            buildingCall(phoneData);
+        } else {
+            Log.w(TAG, "initBuildingCalls: lastLocation is null!");
+        }
+        handler.postDelayed(runnable, DELAY);
+    }
+
+
     private void buildingCall(PhoneData phoneData) {
-//        Call<Result<Building>> call = virtualCampusWalk.getBuilding(new PhoneData(positionSensorService.getPhoneRotation().getRoll(), phoneLocation));
-        Call<Result<Building>> call = virtualCampusWalk.getBuilding(phoneData);
+        Call<Result<Building>> call = virtualCampusWalk.postBuildingForAchievement(phoneData);
         call.enqueue(new Callback<Result<Building>>() {
             @Override
             public void onResponse(Call<Result<Building>> call, Response<Result<Building>> response) {
-                Result<Building> body = response.body();
-                Log.i(TAG, "RESPONSE: " + response.body().toString());
+                if (response.isSuccessful() && response.body().isSuccess()) {
+                    Result<Building> body = response.body();
+                    Log.i(TAG, "Received response for building call!: " + response.body().toString());
+                    fillDataFrame(body);
+                }
             }
 
             @Override
             public void onFailure(Call<Result<Building>> call, Throwable throwable) {
-                Log.e(TAG, "ERROR: " + throwable.getMessage(), throwable);
+                Log.e(TAG, "Received error!: " + throwable.getMessage(), throwable);
+                setDataFrameVisibility(false);
             }
         });
     }
@@ -92,13 +123,15 @@ public class GameCameraViewFragment extends GamePositionServiceFragment {
     @Override
     public void onResume() {
         super.onResume();
-//        initCamera();
+        initCamera();
+        runnable.run();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-//        stopCamera();
+        stopCamera();
+        handler.removeCallbacks(runnable);
     }
 
     @Override
@@ -132,10 +165,10 @@ public class GameCameraViewFragment extends GamePositionServiceFragment {
 
     private void initLocationListener() {
         this.locationListener = new LocationListener() {
+
             @Override
             public void onLocationChanged(Location location) {
-                PhoneRotation phoneRotation = positionSensorService.getPhoneRotation();
-                Log.i(TAG, "PHONE ROTATION " + phoneRotation + " NEW LOCATION " + "LAT: " + location.getLatitude() + " LON: " + location.getLongitude());
+                lastLocation = location;
             }
         };
     }
@@ -153,7 +186,7 @@ public class GameCameraViewFragment extends GamePositionServiceFragment {
         setFocus(camera, Camera.Parameters.FOCUS_MODE_AUTO);
     }
 
-    private void initVirtualCampusGame() {
+    private void initVirtualCampusWalk() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -167,4 +200,48 @@ public class GameCameraViewFragment extends GamePositionServiceFragment {
         preview.stopPreviewAndFreeCamera();
     }
 
+    private void fillDataFrame(Result<Building> body) {
+        if (body.isSuccess()) {
+            setDataFrameVisibility(true);
+            setTextViewText(R.id.facultyTextView, body.getValue().getName().toUpperCase());
+
+            List<String> buildingData = new ArrayList<>();
+            buildingData.add(body.getValue().getDescription());
+            buildingData.add(body.getValue().getAddress());
+
+            setListViewData(buildingData);
+            setImageData(body);
+        } else {
+            setDataFrameVisibility(false);
+        }
+    }
+
+    private void setTextViewText(int id, String text) {
+        try {
+            TextView textView = (TextView) getActivity().findViewById(id);
+            textView.setText(text);
+        } catch (Exception ex) {
+            Log.e(TAG, "CameraViewFragment: setTextViewText: " + ex.getMessage(), ex);
+        }
+    }
+
+    private void setListViewData(List<String> buildingData) {
+        ListView listView = (ListView) getActivity().findViewById(R.id.facultyDataListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_view_row, buildingData);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    private void setImageData(Result<Building> body) {
+        String imageBytes = body.getValue().getFaculties().get(0).getLogo().getContent();
+        facultyLogo.setImageBitmap(Util.convertStringByteToBitmap(imageBytes));
+        setDataFrameVisibility(true);
+    }
+
+    private void setDataFrameVisibility(boolean mustBeVisible) {
+        if (mustBeVisible) {
+            getActivity().findViewById(R.id.dataFacultyFrame).setVisibility(View.VISIBLE);
+        } else {
+            getActivity().findViewById(R.id.dataFacultyFrame).setVisibility(View.INVISIBLE);
+        }
+    }
 }
