bb93714555951e6f12d953a8dc0968d8720bd9e2
AdamPiech <adampiech7@op.pl>
2016-12-08 19:21:04 +0100

    Prepared Achievements model

--- app/src/main/java/adm/virtualcampuswalk/fragments/game/GameAchievementsFragment.java
+++ app/src/main/java/adm/virtualcampuswalk/fragments/game/GameAchievementsFragment.java
@@ -1,15 +1,34 @@
 package adm.virtualcampuswalk.fragments.game;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import java.util.List;
+
 import adm.virtualcampuswalk.R;
 import adm.virtualcampuswalk.fragments.PositionServiceFragment;
+import adm.virtualcampuswalk.models.Achievement;
+import adm.virtualcampuswalk.models.MacDto;
+import adm.virtualcampuswalk.models.PhoneData;
+import adm.virtualcampuswalk.models.Result;
+import adm.virtualcampuswalk.utli.api.VirtualCampusWalk;
+import adm.virtualcampuswalk.utli.network.MacReader;
+import adm.virtualcampuswalk.utli.network.SimpleMacReader;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static adm.virtualcampuswalk.utli.Util.BASE_URL;
+import static adm.virtualcampuswalk.utli.Util.TAG;
 
 /**
  * Created by Adam Piech on 2016-12-07.
@@ -17,17 +36,52 @@ import adm.virtualcampuswalk.fragments.PositionServiceFragment;
 
 public class GameAchievementsFragment extends PositionServiceFragment {
 
+    private VirtualCampusWalk virtualCampusWalk;
+    private MacReader mac = new SimpleMacReader();
+
     private TextView titleTextView;
     private ImageView medalImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View view = inflater.inflate(R.layout.achievements_activity, container, false);
-        titleTextView = (TextView) view.findViewById(R.id.achievementTitle);
-        medalImageView = (ImageView) view.findViewById(R.id.achievementImage);
-        setAchievement("Zbierz wszystkie zanczniki", R.mipmap.gold_medal);
-        return view;
+        View inflate = inflater.inflate(R.layout.achievements_activity, container, false);
+        initVirtualCampusWalk();
+
+        titleTextView = (TextView) inflate.findViewById(R.id.achievementTitle);
+        medalImageView = (ImageView) inflate.findViewById(R.id.achievementImage);
+        setAchievement("Zbierz wszystkie znaczniki", R.mipmap.gold_medal);
+
+        return inflate;
+    }
+
+    private void call(PhoneData phoneData) {
+        Call<Result<List<Achievement>>> achievements = virtualCampusWalk.getAchievements(new MacDto(mac.getMacAddress()));
+        achievements.enqueue(
+                new Callback<Result<List<Achievement>>>() {
+                    @Override
+                    public void onResponse(Call<Result<List<Achievement>>> call, Response<Result<List<Achievement>>> response) {
+                        Log.i(TAG, "onResponse: " + response.isSuccessful() + " " + response.body());
+                        if (response.isSuccessful() && response.body().isSuccess()) {
+                            for (Achievement achievement : response.body().getValue()) {
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Result<List<Achievement>>> call, Throwable throwable) {
+                        Log.e(TAG, "getAchievements: ", throwable);
+                    }
+                }
+        );
+    }
+
+    private void initVirtualCampusWalk() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+        virtualCampusWalk = retrofit.create(VirtualCampusWalk.class);
     }
 
     private void setAchievement(String title, Integer medal) {
@@ -48,12 +102,8 @@ public class GameAchievementsFragment extends PositionServiceFragment {
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
-    }
-
-    @Override
     public void onDestroy() {
         super.onDestroy();
     }
+
 }
